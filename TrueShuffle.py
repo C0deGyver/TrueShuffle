67ed7324a9115f630ed1bed5a558ce888327aee4
C0deGyver <davidd.okc@gmail.com>
2019-02-09 23:39:55 -0600

    pre-versioning guess work...

--- TrueShuffle.py
+++ TrueShuffle.py
@@ -1,5 +1,5 @@
 #!/usr/bin/env python2.7
-# Version 0.0.1
+# Version 0.0.2
 # pre-versioning guess work
 
 # import items
@@ -12,21 +12,28 @@ try:
 
 	# setup pin number associations to buttons
 	pin = 11
+	pin2 = 12
 
 	# buttons must be wired to 3v3 and pin in with a pull down
 	GPIO.setup(pin, GPIO.IN)
+	GPIO.setup(pin2, GPIO.IN)
 
 	# add event detection
 	GPIO.add_event_detect(pin, GPIO.RISING)
+	GPIO.add_event_detect(pin2, GPIO.RISING)
 	
 	while True:
 		if GPIO.event_detected(pin):
 			print("Pressed!")
-			GPIO.remove_event_detect(pin)
+
+		if GPIO.event_detected(pin2):
+			print("Pressed!")
 
 except KeyboardInterrupt:
 	print("Keyboard Interrupt!")
 
 finally:
 	# clean gpio
+	GPIO.remove_event_detect(pin)
+	GPIO.remove_event_detect(pin2)
 	GPIO.cleanup()
