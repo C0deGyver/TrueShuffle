cce1c24de73cce3961c13a66b81a32716f4b982e
C0deGyver <davidd.okc@gmail.com>
2019-02-10 17:57:08 -0600

    pre-version work... trying to figure out how callbacks work

--- TrueShuffle.py
+++ TrueShuffle.py
@@ -1,5 +1,5 @@
 #!/usr/bin/env python2.7
-# Version 0.0.3
+# Version 0.0.4
 # pre-versioning guess work
 
 # import items
@@ -13,22 +13,28 @@ try:
 	# setup pin number associations to buttons
 	pin = 11
 	pin2 = 12
+	pin3 = 13
 
 	# buttons must be wired to 3v3
-	# using builtin pull-down resistors now :)
+	# using builtin pull-down resistors
 	GPIO.setup(pin, GPIO.IN, pull_up_down=GPIO.PUD_DOWN)
 	GPIO.setup(pin2, GPIO.IN, pull_up_down=GPIO.PUD_DOWN)
+	GPIO.setup(pin3, GPIO.IN, pull_up_down=GPIO.PUD_DOWN)
+
+	# callback functions for events
+	def pinFunc():
+		print("Pressed!")
+
+	def pin2Func():
+		print("Pressed 2!")
 
 	# add event detection
-	GPIO.add_event_detect(pin, GPIO.RISING)
-	GPIO.add_event_detect(pin2, GPIO.RISING)
+	GPIO.add_event_detect(pin, GPIO.RISING, callback=pinFunc)
+	GPIO.add_event_detect(pin2, GPIO.RISING, callback=pin2Func)
 
-	while True:
-		if GPIO.event_detected(pin):
-			print("Pressed!")
-
-		if GPIO.event_detected(pin2):
-			print("Pressed2!")
+	# getting rid of the endless loop to free the proc up
+	GPIO.wait_for_edge(pin3, GPIO.RISING)
+	print("Pressed 3. Stopping program")
 
 except KeyboardInterrupt:
 	print("Keyboard Interrupt!")
@@ -37,4 +43,5 @@ finally:
 	# clean gpio
 	GPIO.remove_event_detect(pin)
 	GPIO.remove_event_detect(pin2)
+	GPIO.remove_event_detect(pin3)
 	GPIO.cleanup()
