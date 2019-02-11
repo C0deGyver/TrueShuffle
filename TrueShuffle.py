0309e21674be83c210cee27eca7652ce91db5f8a
C0deGyver <davidd.okc@gmail.com>
2019-02-10 18:24:08 -0600

    pre-version work making theprogram more visual by "logging" major events

--- TrueShuffle.py
+++ TrueShuffle.py
@@ -1,16 +1,19 @@
 #!/usr/bin/env python2.7
-# Version 0.1.1
+# Version 0.1.2
 # pre-versioning guess work
 
 # import items
 import RPi.GPIO as GPIO
 
+print("Running TrueShuffle.py")
 
 try:
         # Setting GPIO mode
+        print("Setting GPIO mode")
         GPIO.setmode(GPIO.BOARD)
 
         # setup pin number associations to buttons
+        print("Setting pin numbers")
         pin = 11
         pin2 = 12
         pin3 = 13
@@ -20,6 +23,7 @@ try:
 
         # buttons must be wired to 3v3
         # using builtin pull-down resistors
+        print("Setting up pins to be pulled down to ground")
         GPIO.setup(pin, GPIO.IN, pull_up_down=GPIO.PUD_DOWN)
         GPIO.setup(pin2, GPIO.IN, pull_up_down=GPIO.PUD_DOWN)
         GPIO.setup(pin3, GPIO.IN, pull_up_down=GPIO.PUD_DOWN)
@@ -28,6 +32,7 @@ try:
         GPIO.setup(pin6, GPIO.IN, pull_up_down=GPIO.PUD_DOWN)
 
         # callback functions for events
+        print("Defining callbacks for the interrupts")
         def pinFunc(channel):
                 print("Pressed!")
 
@@ -44,6 +49,7 @@ try:
                 print("Pressed 5!")
 
         # add event detection
+        print("Adding event detection for the buttons")
         GPIO.add_event_detect(pin, GPIO.RISING, callback=pinFunc)
         GPIO.add_event_detect(pin2, GPIO.RISING, callback=pin2Func)
         GPIO.add_event_detect(pin3, GPIO.RISING, callback=pin3Func)
@@ -56,6 +62,7 @@ try:
 
 finally:
         # clean gpio
+        print("Cleaning GPIO")
         GPIO.remove_event_detect(pin)
         GPIO.remove_event_detect(pin2)
         GPIO.remove_event_detect(pin3)
@@ -63,3 +70,4 @@ finally:
         GPIO.remove_event_detect(pin5)
         GPIO.remove_event_detect(pin6)
         GPIO.cleanup()
+        print("TrueShuffle.py finished!")
